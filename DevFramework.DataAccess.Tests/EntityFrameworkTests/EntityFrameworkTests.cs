cbaa1145296eb3ab132549310a9977bb39ceb82a
MeteAnn <meeteann@gmail.com>
2025-11-17 13:43:40 +0300

    BirimTestiYazilmasi2

--- DevFramework.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTests.cs
+++ DevFramework.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTests.cs
@@ -37,7 +37,23 @@ namespace DevFramework.DataAccess.Tests.EntityFrameworkTests
 
         }
 
+        [TestMethod]
+
+        public void Get_all_with_parameter_returns_filtered_products() 
+        {
+
+            EfProductDal productDal = new EfProductDal();
+            var result = productDal.GetList(p=>p.ProductName.Contains("ab"));
 
 
-    }
+            Assert.AreEqual(85, result.Count);
+
+
+
+
+        }
+
+
+
+
 }
