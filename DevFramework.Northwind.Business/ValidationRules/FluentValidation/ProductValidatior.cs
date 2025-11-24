e4be22097066191c3c17d83a36c21fa91599915a
MeteAnn <meeteann@gmail.com>
2025-11-24 15:14:57 +0300

    İş katmanının implemantasyonu

---
+++ DevFramework.Northwind.Business/ValidationRules/FluentValidation/ProductValidatior.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ValidatorTool
+    {
+
+        public static void Validate(IValidator validator, object entity)
+        {
+           
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+
+
+    }
+}
