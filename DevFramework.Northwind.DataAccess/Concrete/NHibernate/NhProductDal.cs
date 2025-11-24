6fbff0f25e22a99d605d6aa5c25e877b59e7ea33
MeteAnn <meeteann@gmail.com>
2025-11-24 14:51:05 +0300

    Nhibernate Complex Tipler

--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -13,20 +13,33 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
     public class NhProductDal:NhEntityRepositoryBase<Product>, IProductDal
 
     {
-
+        private NhibernateHelper _nhibernateHelper;
 
 
         public NhProductDal(NhibernateHelper nhibernateHelper):base(nhibernateHelper)
         {
 
-
-
             
+            _nhibernateHelper = nhibernateHelper;
+
+
         }
 
         public List<ProductDetail> GetProductDetails()
         {
-            throw new NotImplementedException();
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>()
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName
+                             };
+                return result.ToList();
+            }
         }
     }
 }
